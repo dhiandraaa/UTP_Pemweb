703887ee8150fe3cd7f2465bd6850213e26e544a
Dhiandra Adithya Agsatra <d.adithya.a@gmail.com>
2018-05-14 06:30:03 +0700

    Update bayar-proses.php

--- bayar-proses.php
+++ bayar-proses.php
@@ -8,10 +8,10 @@ $jenis = $_POST['jenis_zakat'];
 if(isset($_POST['Submit'])){
     $insert = mysqli_query($kon, "INSERT INTO laporan(nama,jenis_zakat,jml_zakat) VALUES ('$nama','$jenis',$hasil)");
     if($insert){
-        echo "Data Berhasil Ditambahkan Ke Database";
-    }else{
-        echo"Gagal";
+        echo "Data Berhasil Ditambahkan ke Database";
+    }
+    else{
+        echo"Data Gagal Ditambahkan ke Database";
     }
 }
-
 ?>
