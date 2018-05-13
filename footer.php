f069a2c63e34946103c79e6d614a00fc8a5c85fc
Bisri Mustofa <37740712+Bisbiss@users.noreply.github.com>
2018-05-13 20:55:34 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,18 +1,43 @@
 <!DOCTYPE html>
-<html>
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/slim.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+</head>
 <body>
-    <div class="container-fluid footer">
-        <div class"container">
+    <div class="container-fluid">
+        <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    <p> Nama Anggota : Bisri Mustofa 1617051092 A Dhiandra Adithya Agsatra 1617051108 A Megi Aji Pangestu 1617051124 A </p>
+                <div class="col-md">
+                <hr>
+               <h2> Find Us : </h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-128.png" width="175px" height="175px" class="img-responsive">
+                </div>
+                <div class="col-sm-4">
+                    <img src="https://www.freeiconspng.com/uploads/facebook-png-02-15.png" width="175px" height="175px" class="img-responsive">
+                </div>
+                <div class="col-sm-4">
+                    <img src="https://images.vexels.com/media/users/3/131144/isolated/preview/390a6b6e2a83f88a266d3ab5b81fc3ce-instagram-square-logo-by-vexels.png" width="175px" height="175px" class="img-responsive">
                 </div>
             </div>
         </div>
     </div>
-
+    <div class="container-fluid bawah">
+        <div class="container">
+            <div class="row">
+            <center>
+                <h4>Design By Pemweb</h4>
+            </center>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
