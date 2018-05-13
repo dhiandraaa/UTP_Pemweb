d96cd21e02d6dea6ffa02a747f7db4041ead203c
Bisri Mustofa <37740712+Bisbiss@users.noreply.github.com>
2018-05-13 14:35:51 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,28 +1,32 @@
+<?php
+require('header.html');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Login Page</title>
 </head>
-<body>
-    <form action="" method="POST">
-        <input type="text" name="user" placeholder="Username" value="">
-        <input type="password" name="pasw" placeholder="Password" value="">
-        <button type="submit" name="login">LOGIN</button>
-    </form>
+<body> <center>
+    <div class="container">
+        <div class="row">
+            <div class="col-md">
+                <form action="login_press.php" method="POST">
+                    <div class="form-group">
+                        <label for="user">Username</label>
+                        <input type="text" class="form-control" id="user" name="nama" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" name="pass" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <button type="submit" name="save" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</center>
 </body>
 </html>
-
-<?php
-$akun = 'su';
-$pas = 'su';
-if(isset($_POST['login'])){
-    if($akun == $_POST['user'] && $pas==$_POST['pasw']){
-        header("location:dash.php");
-    }else{
-        echo "Login Gagal";
-    }
-}
-?>
