b3bfe9b2c591a831e6906ba0c4c48d614cb2dd3d
megiajip <34880686+megiajip@users.noreply.github.com>
2018-05-14 13:27:01 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,18 +14,21 @@
             <div class="row">
                 <div class="col-md">
                 <hr>
-               <h2> Find Us : </h2>
+                  <h2> Find Us : </h2>
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-4">
-                    <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-128.png" width="175px" height="175px" class="img-responsive">
+                    <a href="https://github.com/dhiandraaa/UTP_Pemweb">
+                      <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-128.png" width="90px" height="90px" class="img-responsive">
                 </div>
                 <div class="col-sm-4">
-                    <img src="https://www.freeiconspng.com/uploads/facebook-png-02-15.png" width="175px" height="175px" class="img-responsive">
+                    <a href="https://www.facebook.com/megiajip">
+                      <img src="https://www.freeiconspng.com/uploads/facebook-png-02-15.png" width="90px" height="90px" class="img-responsive">
                 </div>
                 <div class="col-sm-4">
-                    <img src="https://images.vexels.com/media/users/3/131144/isolated/preview/390a6b6e2a83f88a266d3ab5b81fc3ce-instagram-square-logo-by-vexels.png" width="175px" height="175px" class="img-responsive">
+                    <a href="http://www.instagram.com/bisrimustofa_frame">
+                      <img src="https://images.vexels.com/media/users/3/131144/isolated/preview/390a6b6e2a83f88a266d3ab5b81fc3ce-instagram-square-logo-by-vexels.png" width="96px" height="96px" class="img-responsive">
                 </div>
             </div>
         </div>
@@ -33,11 +36,12 @@
     <div class="container-fluid bawah">
         <div class="container">
             <div class="row">
-            <center>
-                <h4>Design By Pemweb</h4>
-            </center>
+              <hr>
+                <center><h4 style="color:black">Powered By Pemweb</h4></center>
             </div>
+          </hr>
         </div>
     </div>
+  </div>
 </body>
 </html>
