cbb5d45470d687123aff2c8ef3bf742c550eb29b
megiajip <34880686+megiajip@users.noreply.github.com>
2018-05-14 13:25:04 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Zakat</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+
+    <script src="js/slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <body>
+      <div class="navigasi">
+        <ul class="navigasi">
+        <li><b><a class="active" href="index.php">Home</a></b></li>
+        <li><b><a href="kalkulator.php">Hitung Zakat</a></b></li>
+        <li><b><a href="about.php">About</a></b></li>
+        <li><b><a href="logout.php">Logout</a></b></li>
+      </ul>
+      </div>
+      <div class="container-fluid tengah">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg">
+              <center><h2>ABOUT ME<br><br><br></h2></center>
+          </div>
+          </div>
+          <div id=foto>
+          <div class="row">
+            <div class="col-md-4">
+            <table>
+            <tr><h2><center> &nbsp;&nbsp;&nbsp; Dhiandra Adhithya A </center></h2></tr>
+            <tr><h6>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Contact Person : 081923912312</h6></tr>
+            </table>
+            </div>
+            <div class="col-md-4">
+            <table>
+              <tr><h2><center> &nbsp;&nbsp;&nbsp; Bisri Mutofa </center></h2></tr>
+              <tr><h6><center>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Contact Person : 081923912312</center></h6></tr>            </table>
+            </div>
+            <div class="col-md-4">
+               <table>
+                 <tr><h2><center> &nbsp;&nbsp;&nbsp; Megi Aji P </center></h2></tr>
+                 <tr><h6><center>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Contact Person : 081923912312</center></h6></tr>            </table>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+      <br><br><br><br><br><br><br>
+  </body>
+</html>
+
+<?php
+    require("footer.php");
+?>
