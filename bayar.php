8610e37547ab65a469d40228df666a6e97e7af46
Dhiandra Adithya Agsatra <d.adithya.a@gmail.com>
2018-05-14 12:43:39 +0700

    Update bayar.php

--- bayar.php
+++ bayar.php
@@ -9,7 +9,6 @@
     require("header.php");
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,16 +18,25 @@
     <title>Bayar Zakat</title>
 </head>
 <body>
+    <div>
+    <h6 class="section-title h1">Bayar Zakat</h6>
     <center>
     <form action="bayar-proses.php" method="POST">
-        <input type="text" name="jml" Placeholder="Jumlah Zakat"><br>
-        <input type="text" name="nama" Placeholder="Nama"> <br>
-        <input type="radio" name="jenis_zakat" value="Zakat Fitrah" checked> Zakat Fitrah<br>
-        <input type="radio" name="jenis_zakat" value="Zakat Mal">Zakat Mal<br>
-        <input type="radio" name="jenis_zakat" value="Zakat Profesi">Zakat Profesi<br>
+        <table>
+            <tr>
+            <td>
+                <input type="text" name="jml" Placeholder="Jumlah Zakat"> <br>
+                <input type="text" name="nama" Placeholder="Nama"> <br>
+                <input type="radio" name="jenis_zakat" value="Zakat Fitrah" checked> Zakat Fitrah<br>
+                <input type="radio" name="jenis_zakat" value="Zakat Mal">Zakat Mal<br>
+                <input type="radio" name="jenis_zakat" value="Zakat Profesi">Zakat Profesi<br>
+            </td>
+            </tr>
+        </table>
         <button type="submit" name="Submit">Bayar</button>
     </form>
-    </center>    
+    </center>   
+    </div> 
 </body>
 </html>
 
